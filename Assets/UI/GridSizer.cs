c821880cbebaaf800c1264d0a9fedcad9ee42dd8
unknown <azfougues@gmail.com>
2023-11-12 12:42:55 -0400

    Made design responsive

--- Assets/UI/GridSizer.cs
+++ Assets/UI/GridSizer.cs
@@ -11,7 +11,9 @@ public class GridSizer : MonoBehaviour{
     }
 
     private void Start() {
+        Vector2 spacingRatio = new Vector2(grid.spacing.x / grid.cellSize.x, grid.spacing.y / grid.cellSize.y);
         grid.cellSize = new Vector2(Camera.main.aspect > 1 ? Screen.height : Screen.width - (grid.padding.left + grid.padding.right), grid.cellSize.y);
+        grid.spacing = new Vector2(grid.cellSize.x * spacingRatio.x, grid.cellSize.y * spacingRatio.y);
     }
 
     private void Update() {
